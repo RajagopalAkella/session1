acb11d188fe9b63434292a91faf274cf67091c18
RajagopalAkella <akellaraj2@gmail.com>
2022-09-19 16:49:44 +0530

    Feature1Cmp

--- force-app/main/default/aura/accountContactsCmp/accountContactsCmpController.js
+++ force-app/main/default/aura/accountContactsCmp/accountContactsCmpController.js
@@ -3,7 +3,9 @@
         cmp.set('v.mycolumns', [
                 {label: 'Contact Name', fieldName: 'Name', type: 'text'},
                 {label: 'Phone', fieldName: 'Phone', type: 'phone'},
-                {label: 'Email', fieldName: 'Email', type: 'email'}
+                {label: 'Email', fieldName: 'Email', type: 'email'},
+            	{label: 'Description', fieldName: 'Description', type: 'text'},
+            	{label: 'CustomField', fieldName: 'CustomField1__c', type: 'text'}
             ]);
         helper.getData(cmp);
     }
